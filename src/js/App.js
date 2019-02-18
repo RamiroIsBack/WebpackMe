e547bbffd7948538a6e3e65757fbece57ab2acb4
ra <ramiro.santamaria.alvarez@gmail.com>
2019-02-18 10:31:42 -0500

    very bacin set up for SASS and REACT

---
+++ src/js/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1 className="header_container">Hellos SaS world</h1>
+          <p className="paragraph_container">
+            {" "}
+            yes this is a brand new app to practice and use SASS more in depth
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
